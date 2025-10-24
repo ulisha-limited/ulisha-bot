e433f36380f865e4b9da4d5e92be5bebb5a44205
Melvin Jones Repol <mrepol742@gmail.com>
2025-10-24 17:33:28 +0800

    chore: add supabase client and types

---
+++ src/components/supabase.ts
@@ -0,0 +1,16 @@
+/**
+ * Required Notice: Copyright (c) 2025 Ulisha Limited (https://www.ulishalimited.com)
+ *
+ * This file is licensed under the Polyform Noncommercial License 1.0.0.
+ * You may obtain a copy of the License at:
+ *
+ *     https://polyformproject.org/licenses/noncommercial/1.0.0/
+ */
+
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "../supabase-types";
+
+export const supabaseAdmin = createClient<Database>(
+  process.env.SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || "",
+);
